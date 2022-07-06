e1ef76a185b89b91531646358ac6936eb9188777
jinyang <1608918778@qq.com>
2022-07-06 14:27:04 +0800

    Initial commit

---
+++ src/main/java/com/zjydemo/mallstore/entity/NewEntity.java
@@ -0,0 +1,13 @@
+package com.zjydemo.mallstore.entity;
+
+/**
+ * @author zjy
+ * @version 1.0
+ */
+
+public class NewEntity extends BaseEntity{
+    public NewEntity() {
+        super();
+        System.out.println("construct");
+    }
+}
